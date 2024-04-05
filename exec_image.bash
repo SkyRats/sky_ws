ab344d91d786642c90ce695efa8f541184f6532c
Caio <83253047+hfcaio@users.noreply.github.com>
2024-04-05 09:05:21 -0300

    Update exec_image.bash

--- exec_image.bash
+++ exec_image.bash
@@ -1,12 +1,10 @@
-xhost local: root
-XAUTH=/tmp/.docker.xauth
+xhost +
 
 sudo docker start sky_ws bash
 
 sudo docker exec -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
-    --env="XAUTHORITY=$XAUTH" \
     --privileged \
     sky_ws \
     bash 
