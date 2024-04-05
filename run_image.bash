2b5cb170a658972c4f5648b8d3b36d6d286fcf62
Caio <83253047+hfcaio@users.noreply.github.com>
2024-04-05 09:03:44 -0300

    Update run_image.bash

--- run_image.bash
+++ run_image.bash
@@ -1,12 +1,8 @@
-xhost local: root
-XAUTH=/tmp/.docker.xauth
-
+xhost +
 sudo docker run -it \
     --name=sky_ws \
     --env="DISPLAY=$DISPLAY" \
     --volume="/tmp/.x11-unix:/tmp/.x11-unix:rw" \
-    --env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XATH" \
     --volume="<absolute/path/to/sky_ws>:/home/sky/sky_ws" \
     --net=host \
     --privileged \
